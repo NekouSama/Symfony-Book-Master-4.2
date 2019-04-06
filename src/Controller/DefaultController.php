f9107239ea037a13d713fffea72866de436bea68
Jérôme BALIEUX-SCHOENECKER <jschoenecker57@gmail.com>
2019-04-06 17:36:32 +0200

    feat: Add a logout's method + Add bootstrap4 layout for forms + Formatting and Link

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,11 +15,11 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        //var_dump($this->getUser()->getEmail());
-        // Square Shape, Size 64px
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'email' => $this->getUser()->getEmail()
         ]);
     }
 }
