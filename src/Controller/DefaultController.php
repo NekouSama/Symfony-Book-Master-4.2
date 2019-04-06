d9f7336c16d1113a026cacb1edf4cb21c33e9400
Jérôme BALIEUX-SCHOENECKER <jschoenecker57@gmail.com>
2019-04-06 20:13:53 +0200

    feat: add embed controller for navbar + add avatar for user

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -20,6 +21,18 @@ class DefaultController extends AbstractController
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'email' => $this->getUser()->getEmail()
+        ]);
+    }
+
+    public function navbar()
+    {
+        if ($this->getUser()) {
+            $image_profil = getenv('USER_AVATAR_DIRECTORY') . $this->getUser()->getId() . '.png';
+        }
+
+        return $this->render('navbar.html.twig', [
+            'user' => $this->getUser(),
+            'image_profil' => $image_profil ?? null
         ]);
     }
 }
